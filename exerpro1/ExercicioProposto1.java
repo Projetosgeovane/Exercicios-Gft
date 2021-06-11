0d7da73d9f01edba6556a2c20f3b07a7e48490c5
Geovane Silva <geovanesilva.nar@hotmail.com>
2021-06-11 00:52:04 -0300

    Exercício proposto 1

---
+++ exerpro1/ExercicioProposto1.java
@@ -0,0 +1,57 @@
+package exerpro1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ExercicioProposto1 {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        List<Double> temperaturasemestral = new ArrayList<>();
+
+        double soma = 0.0;
+        for(int i=1; i<= 6; i++){
+            System.out.println("Qual a temperatura do mês " + i + ": ");
+            double temp = teclado.nextDouble();
+            temperaturasemestral.add(temp);
+            soma += temp;
+        }
+
+        double temperaturamediasemestral = soma/temperaturasemestral.size();
+
+        System.out.println("Temperatura Semestral: " + temperaturamediasemestral);
+        System.out.println("Média temperatura Semestral: " + temperaturamediasemestral);
+        System.out.println("-----------" );
+        System.out.println("Temperaturas maiores que a média:");
+
+        for(Double temp : temperaturasemestral){
+            if(temp > temperaturamediasemestral){
+                int index = temperaturasemestral.indexOf(temp);
+                switch (index){
+                    case 0:
+                        System.out.println((index + 1) + " - Janeiro: " + temp + " oC");
+                        break;
+
+                    case 1:
+                        System.out.println((index + 1) + " - Fevereiro" + temp + " oC");
+                        break;
+
+                    case 2:
+                        System.out.println((index + 1) + " - MARÇO: " + temp+ " oC");
+                        break;
+                    case 3:
+                        System.out.println((index + 1) + " - ABRIL: " + temp+ " oC");
+                        break;
+                    case 4:
+                        System.out.println((index + 1) + " - MAIO: " + temp+ " oC");
+                        break;
+                    case 5:
+                        System.out.println((index + 1) + " - JUNHO: " + temp+ " oC");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
--- ordenacaoList/ExemploOrdenacaoList.java
+++ ordenacaoList/ExemploOrdenacaoList.java
@@ -1,4 +1,4 @@
-package OrdenacaoList;
+package ordenacaoList;
 
 import java.util.ArrayList;
 import java.util.Collections;
